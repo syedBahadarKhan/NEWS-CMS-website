af12c408a258ec35972844ba76bbac687c632ce6
syedBahadarKhan <ba4646465@gmail.com>
2025-12-25 00:31:48 +0500

    The Env files has been protected"

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 import express from "express"
 const app = express()
-const Port = 3000;
+import mongoose from "mongoose"
+import path from "path"
+import expressLayouts from "express-ejs-layouts"
+import session from "express-session"
+import flash from "connect-flash"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(expressLayouts)
+app.set("layout", "layout")
+
+
+//View Engine 
+app.set("view engine", "ejs")
+
+
+//Database connection
+mongoose.connect(process.env.MONGODB_URI)
 
 
 app.get('/', (req, res) =>{
