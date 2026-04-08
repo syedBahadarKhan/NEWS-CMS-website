22b288db999ee4991d1960b461071eea96703ad4
syedBahadarKhan <ba4646465@gmail.com>
2026-04-08 11:04:09 +0500

    the error handling for frontend is start

--- routes/Frontend.js
+++ routes/Frontend.js
@@ -12,4 +12,12 @@ router.get("/search", frontendController.search);
 router.get("/author/:id", frontendController.author);
 router.post("/single/:id/comment", frontendController.addComment);
 
+
+
+router.use((req, res, next) =>{
+    res.status(404).render("404",{
+        message:"Pagr Not Found"
+    })
+})
+
 export default router;
